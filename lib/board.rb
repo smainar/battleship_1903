ef7f67182d3da208b84e7d70a5ea3be0afd2f523
Stella Mainar <mainars09@alumni.hanover.edu>
2019-03-21 14:13:47 -0600

    Refactor cells method and add same row and adjacent method for board class
    Co-authored-by: Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Board
   attr_reader :cells
 
@@ -6,23 +8,23 @@ class Board
   end
 
   def cells
-    @cells = {
-        "A1" => Cell.new("A1"),
-        "A2" => Cell.new("A2"),
-        "A3" => Cell.new("A3"),
-        "A4" => Cell.new("A4"),
-        "B1" => Cell.new("B1"),
-        "B2" => Cell.new("B2"),
-        "B3" => Cell.new("B3"),
-        "B4" => Cell.new("B4"),
-        "C1" => Cell.new("C1"),
-        "C2" => Cell.new("C2"),
-        "C3" => Cell.new("C3"),
-        "C4" => Cell.new("C4"),
-        "D1" => Cell.new("D1"),
-        "D2" => Cell.new("D2"),
-        "D3" => Cell.new("D3"),
-        "D4" => Cell.new("D4")
+    {
+      "A1" => Cell.new("A1"),
+      "A2" => Cell.new("A2"),
+      "A3" => Cell.new("A3"),
+      "A4" => Cell.new("A4"),
+      "B1" => Cell.new("B1"),
+      "B2" => Cell.new("B2"),
+      "B3" => Cell.new("B3"),
+      "B4" => Cell.new("B4"),
+      "C1" => Cell.new("C1"),
+      "C2" => Cell.new("C2"),
+      "C3" => Cell.new("C3"),
+      "C4" => Cell.new("C4"),
+      "D1" => Cell.new("D1"),
+      "D2" => Cell.new("D2"),
+      "D3" => Cell.new("D3"),
+      "D4" => Cell.new("D4")
     }
   end
 
@@ -30,31 +32,38 @@ class Board
     @cells.include?(coordinate)
   end
 
+  def valid_placement?(ship, ship_coordinates)
+    valid_ship_length?
+  end
+
   def valid_ship_length?(ship, ship_coordinates)
     ship.length == ship_coordinates.length
   end
 
-  def coordinates_in_the_same_row(ship, ship_coordinates)
+  def same_row_and_adjacent(ship, ship_coordinates)
     # check that 1st character in string is the same
-      #  if the same, 2nd character is sequentialr
+      #  if the same, 2nd character is sequential
     letter_array = ship_coordinates.map do |ship_coordinate|
       ship_coordinate[0]
     end
-    letter_array.uniq.count == 1
+
+    if letter_array.uniq.count == 1
+      number_array = ship_coordinates.map do |ship_coordinate|
+        ship_coordinate[1]
+      end
+      number_array.each_cons(2).all? do |current_num, previous_num|
+          previous_num.to_i == current_num.to_i + 1
+      end
+    end
   end
 
   def coordinates_in_the_same_column(ship, ship_coordinates)
-    # check that 2nd characterin string is same
+    # check that 2nd character in string is same
      # if the same, 1st is sequential
     number_array = ship_coordinates.map do |ship_coordinate|
       ship_coordinate[1]
     end
     number_array.uniq.count == 1
-  end
-
-  def valid_placement?(ship, ship_coordinates)
-    # valid_coordinate?
-    valid_ship_length?
   end
 
 end
