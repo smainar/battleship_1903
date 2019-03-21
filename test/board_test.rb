0403f0e146a12d659a404693cd627948597f6041
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-03-21 17:42:57 -0600

    Add test to ensure ships do not overlap
    Co-authored-by: Stella Mainar <43945779+smainar@users.noreply.github.com>

--- test/board_test.rb
+++ test/board_test.rb
@@ -99,4 +99,10 @@ class BoardTest < Minitest::Test
     assert @cell_3.ship == @cell_2.ship
   end
 
+  def test_ships_cannot_overlap
+    @board.place(@cruiser, ["A1", "A2", "A3"])
+
+    refute @board.valid_placement?(@submarine, ["A1", "B1"])
+  end
+
 end
