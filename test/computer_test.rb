a013e9789a381e060ce8ea91d3767d80fc0079f7
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-03-26 16:11:54 -0600

    Add tests checking that computer starts with 16 possible targets and can fire at each target only once
    Co-authored-by: Stella Mainar <43945779+smainar@users.noreply.github.com>

--- test/computer_test.rb
+++ test/computer_test.rb
@@ -8,8 +8,9 @@ require './lib/computer'
 class ComputerTest < MiniTest::Test
 
   def setup
-    @board    = Board.new
-    @computer = Computer.new(@board, [@cruiser = Ship.new("Cruiser", 3), @submarine = Ship.new("Submarine", 2)])
+    @computer_board = Board.new
+    @player_board   = Board.new
+    @computer       = Computer.new(@computer_board, @player_board, [@cruiser = Ship.new("Cruiser", 3), @submarine      = Ship.new("Submarine", 2)])
   end
 
   def test_it_exists
@@ -19,6 +20,18 @@ class ComputerTest < MiniTest::Test
   def test_by_default_it_has_two_ships
     assert_equal [@cruiser, @submarine], @computer.ships
     assert_equal 2, @computer.ships.count
+  end
+
+  def test_by_default_it_has_an_array_of_16_targets_to_fire_upon
+    assert_equal 16, @computer.targets.count
+  end
+
+  def test_it_can_fire_16_times_at_unique_targets
+    16.times do
+      @computer.fire_at_will(@computer.target)
+    end
+
+    assert_empty @computer.targets
   end
 
   def test_it_can_validate_random_coordinates_for_ships
