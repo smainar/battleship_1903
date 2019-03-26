be560f84b75e4318107c209a0f0a8c80b5527123
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-03-25 19:59:18 -0600

    Rewrite tests for finding random coordinates
    Co-authored-by: Stella Mainar <43945779+smainar@users.noreply.github.com>

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -7,41 +7,46 @@ class ComputerPlayer
     @ships = ships
   end
 
-  def horizontal_cruiser_locations
-    horizontal_groups_of_three.reject! do |coords|
-      coords[0][0] != coords[1][0] || coords[1][0] != coords[2][0]
+  def place_ships
+    @ships.each do |ship|
+      ship_coordinates = find_random_coordinates(ship)
+      @board.place(ship, ship_coordinates)
     end
   end
 
-  def horizontal_groups_of_three
-    horizontal_groups = []
-    @board.cells.keys.each_cons(3) do |coordinates|
-      horizontal_groups << coordinates
+  def find_random_coordinates(ship)
+    ship_coordinates = @board.cells.keys.sample(ship.length)
+    until @board.valid_placement?(ship, ship_coordinates)
+      ship_coordinates = @board.cells.keys.sample(ship.length)
     end
-    horizontal_groups
+    ship_coordinates
   end
 
-  def horizontal_submarine_locations
-    horizontal_groups_of_two.reject! do |coords|
-        coords[0][0] != coords[1][0]
-    end
-  end
-
-  def horizontal_groups_of_two
-    horizontal_groups = []
-    @board.cells.keys.each_cons(2) do |coordinates|
-      horizontal_groups << coordinates
-    end
-    horizontal_groups
-  end
-
-  # def pick_random_cruiser_coordinates
-  #   possible_locations = []
-  #   require "pry"; binding.pry
-  #   @board.cells.keys.each_cons(3) do |coordinates|
-  #     possible_locations << coordinates
+  # def valid_horizontal_cruiser_locations
+  #   horizontal_groups_of_three.reject! do |coords|
+  #     coords[0][0] != coords[1][0] || coords[1][0] != coords[2][0]
   #   end
-  #   possible_locations.sample
   # end
-
+  #
+  # def horizontal_groups_of_three
+  #   horizontal_groups = []
+  #   @board.cells.keys.each_cons(3) do |coordinates|
+  #     horizontal_groups << coordinates
+  #   end
+  #   horizontal_groups
+  # end
+  #
+  # def horizontal_submarine_locations
+  #   horizontal_groups_of_two.reject! do |coords|
+  #     coords[0][0] != coords[1][0]
+  #   end
+  # end
+  #
+  # def horizontal_groups_of_two
+  #   horizontal_groups = []
+  #   @board.cells.keys.each_cons(2) do |coordinates|
+  #     horizontal_groups << coordinates
+  #   end
+  #   horizontal_groups
+  # end
 end
