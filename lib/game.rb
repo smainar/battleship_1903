404288a9764577b2ac5203716f17f3d2ab61b2e2
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-03-27 15:36:57 -0600

    Debug game class so that game ends when a winner is determined
    Co-authored-by: Stella Mainar <43945779+smainar@users.noreply.github.com>

--- lib/game.rb
+++ lib/game.rb
@@ -39,9 +39,8 @@ class Game
   end
 
   def setup_game
-    # Computer can place ships randomly in valid locations
     @computer.place_ships
-    # User can enter valid sequences to place both ships
+
     print "\n"
     puts "I have laid out my ships on the grid."
     puts "You now need to lay out your two ships."
@@ -49,14 +48,13 @@ class Game
     print "\n"
 
     @player_ships.map do |ship|
-      puts "Enter the coordinates for the #{ship.name} (#{ship.length} spaces): "
+      puts "Enter the coordinates for the #{ship.name} (#{ship.length} spaces):"
       user_input = gets.chomp.upcase
       user_input = user_input.split(" ")
       print "\n"
 
-      # Entering invalid ship placements prompts user to enter valid placements
       while @player_board.valid_placement?(ship, user_input) == false
-        puts "Those are invalid coordinates. Please try again: "
+        puts "Those are invalid coordinates. Please try again:"
         user_input = gets.chomp.upcase
         user_input = user_input.split(" ")
         print "\n"
@@ -68,58 +66,43 @@ class Game
   def start_game
     until @winner != nil
 
-    # During the main game, players take turns firing at one another by selecting positions on the grid to attack.
-    #
-    # A single turn consists of:
-
-    # Displaying the boards
-      print "\n\n"
+      print "\n"
       puts "=============COMPUTER BOARD============="
       print @computer_board.render(true)
-
       print "\n"
+
       puts "==============PLAYER BOARD=============="
       print @player_board.render(true)
+      print "\n"
 
-      print "\n\n"
-
-      # Player choosing a coordinate to fire on
-      puts "Enter the coordinate for your shot: "
+      puts "Enter the coordinate for your shot:"
       user_input = gets.chomp.upcase.to_s
       print "\n"
 
       while @computer_board.valid_coordinate?(user_input) == false
-        puts "Please enter a valid coordinate: "
+        puts "Please enter a valid coordinate:"
+        user_input = gets.chomp.upcase.to_s
+        print "\n"
       end
       @computer_board.cells[user_input].fire_upon
 
-      # Computer choosing a coordinate to fire on
       current_target = @computer.target
       @computer.fire_at_will(current_target)
 
-      # Reporting the result of the Player’s shot
-      # @computer.player_board.some_method_name
       puts "Your shot on #{user_input} was a #{@computer_board.cells[user_input].display_results}."
-
-      # Reporting the result of the Computer’s shot
       puts "My shot on #{current_target} was a #{@player_board.cells[current_target].display_results}."
-
       puts "Press enter to continue."
-
       gets
+
+      check_for_winner
     end
-    # continue until there is a winner = all ships sunk
-    check_for_winner
   end
 
   def check_for_winner
-    # if all ships sunk == winner
-    # check player_board
-    # check computer_board
-    # assign winner
-    player_wins = @computer_ships.all? do |ship|
+    player_wins = @computer.computer_ships.all? do |ship|
       ship.sunk?
     end
+
     computer_wins = @player_ships.all? do |ship|
       ship.sunk?
     end
@@ -134,12 +117,15 @@ class Game
 
   def end_game
     print @winner
+    print "\n\n"
+
+    @computer_board = Board.new
+    @player_board   = Board.new
+    @computer       = Computer.new(@computer_board, @player_board)
+    @player_ships   = [Ship.new("Cruiser", 3), Ship.new("Submarine", 2)]
+    @winner         = nil
+
     start
-    # Game ends when all the user’s ships are sunk
-    # Game ends when all the computer’s ships are sunk
-    # Game reports who won
-    # Game returns user back to the Main Menu
-    # The game is over when either the computer or the user sinks all of the enemy ships. When this happens, the user should see a message stating who won
   end
 
 end
